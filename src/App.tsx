3b9c645da81a17c92a7a57962b3ce25614613588
Limarev Ivan <ivantaketo@gmail.com>
2024-05-24 15:55:49 +0600

    Refactor project, fix AddTaskForm.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,67 @@
-import "./App.css";
+import './App.css';
+import React, {useState} from 'react';
+import Task from './components/Task/Task';
+import AddTaskForm from './components/AddTaskForm/AddTaskForm';
+
+interface ITask {
+  id: string;
+  text: string;
+  completed: boolean;
+}
+
+const getId = () => Date.now().toString();
 
 const App = () => {
+  const [tasks, setTasks] = useState<ITask[]>([
+    {text: 'Hello World 1!', id: '1', completed: false},
+    {text: 'Hello World 2!', id: '2', completed: false},
+    {text: 'Hello World 3!', id: '3', completed: false},
+  ]);
+
+  const addNewTask = (event: React.FormEvent<HTMLFormElement>, newPost: string) => {
+    event.preventDefault();
+    if (newPost.length > 3) {
+      const post: ITask = {text: newPost, id: getId(), completed: false};
+
+      setTasks((prevState) => {
+        const copyTasks =  [...prevState];
+        copyTasks.push(post);
+        return copyTasks;
+      });
+    }
+
+  };
+
+  const deleteTask = (id: string) => {
+      setTasks((prevTasks) => {
+        return prevTasks.filter((task) => task.id !== id);
+      });
+  };
+
+  const toggleCompleted = (id: string) => {
+    setTasks((prevTasks) => {
+      return prevTasks.map((task) => {
+        if (task.id === id) {
+          return {...task, completed: !task.completed};
+        }
+
+        return task;
+      });
+    });
+  };
 
   return (
     <div className="App">
-        Hello world
+      <AddTaskForm addNewTask={addNewTask}/>
+      <div className="tasks">
+        {
+          tasks ? (
+            tasks.map(({text, id, completed}) => (
+              <Task key={id} text={text} onRemoveTask={() => deleteTask(id)} completed={completed} onChangeComplete={() => toggleCompleted(id)}/>
+            ))
+          ) : <p>Пока ничего нету</p>
+        }
+      </div>
     </div>
   );
 };
--- src/components/AddTaskForm/AddTaskForm.tsx
+++ src/components/AddTaskForm/AddTaskForm.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
-    getNewTask: React.ChangeEventHandler<HTMLInputElement>;
-    inputValue: string
-    addNewTask: () => void;
+    addNewTask: (event: React.FormEvent<HTMLFormElement>, newPost: string) => void;
 }
 
-const AddTaskForm:React.FC<Props> = ({getNewTask, inputValue, addNewTask}) => {
+const AddTaskForm:React.FC<Props> = ({ addNewTask}) => {
+  const [newPost, setNewPost] = useState<string>('');
+
+  const onFieldChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewPost(event.target.value);
+  };
+
+  const sendTaskForm = (event: React.FormEvent<HTMLFormElement>) => {
+    addNewTask(event, newPost);
+    setNewPost('');
+  };
 
   return (
-        <div className="form-row">
-          <input type="text" placeholder="Add new Task"  value={inputValue} onChange={getNewTask}/>
-          <button type="button" onClick={addNewTask}>Add</button>
-        </div>
+    <form onSubmit={(event) => sendTaskForm(event)}>
+      <div className="form-row">
+        <input type="text" placeholder="Add new Task" value={newPost} onChange={onFieldChange}/>
+        <button type="submit">Add</button>
+      </div>
+    </form>
   );
 };
 
